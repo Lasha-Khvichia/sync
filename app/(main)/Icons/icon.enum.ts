6dd4cac71c27aa4107e17741d52b7b29d26aa84c
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-24 01:26:42 +0400

    feat: add icon enums

---
+++ app/(main)/Icons/icon.enum.ts
@@ -0,0 +1,19 @@
+export enum IconName {
+    add = "add",
+    archive = "archive",
+    camera = "camera",
+    chat = "chat",
+    emoji = "emoji",
+    home = "home",
+    messageSearch = "messageSearch",
+    option = "option",
+    phone = "phone",
+    photo = "photo",
+    rueqvest = "rueqvest",
+    search = "search",
+    shop = "shop",
+    slbum = "slbum",
+    voice = "voice",
+    video = "video",
+}
+  
