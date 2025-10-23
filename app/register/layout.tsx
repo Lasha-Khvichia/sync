3c49495b27bc46925e1ed95bddde13565f792101
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-24 00:54:20 +0400

    feat: add register and login leyout

---
+++ app/register/layout.tsx
@@ -0,0 +1,12 @@
+import "..//globals.css";
+
+export default function RegisterLayout ({children}: {children: React.ReactNode}) {
+
+    return(
+       <html>
+        <body>
+            {children}
+        </body>
+       </html>
+    )
+}
