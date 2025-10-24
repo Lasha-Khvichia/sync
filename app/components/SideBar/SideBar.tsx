a8aafbc004b17c91686745369802b9bedcc30d6e
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-24 22:09:50 +0400

    feart : finished side bar

--- app/components/SideBar/SideBar.tsx
+++ app/components/SideBar/SideBar.tsx
@@ -1,4 +1,64 @@
 import styles from "./SideBar.module.scss";
+
 export default function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <section className={styles.sideBarWrapper}>
+
+      <div className={styles.sideBarContent}>
+
+        <div className={styles.sideBarHeader}>
+
+          <div className={styles.headline}>
+            <h1 className={styles.headlineTitle}>Chat</h1>
+            <div className={styles.headlineIcon}>
+              <button className={styles.btn}>
+                <img src="/icons/video.svg" alt="video" />
+              </button>
+              <button className={styles.btn}>
+                <img src="/icons/add.svg" alt="add" />
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.search}>
+             <img src="/icons/messageSearch.svg" alt="search" className={styles.searchIcon} />
+             <input type="text" placeholder="Search" className={styles.searchInput}/>
+          </div>
+
+        </div>
+
+
+        <div className={styles.sideBarBody}>
+
+           <div className={styles.messageBox}>
+              <img src="/avatar.png" alt="message" className={styles.avatar}/>
+              <div className= {styles.avatarInfo}>
+                  <p className={styles.avatarName}>Rihanna Goldberg</p>
+                  <span className={styles.avatarMessage}>there will be message</span>
+              </div>
+           </div>
+
+           <div className={styles.messageBox}>
+              <img src="/avatar.png" alt="message" className={styles.avatar}/>
+              <div className= {styles.avatarInfo}>
+                  <p className={styles.avatarName}>Rihanna Goldberg</p>
+                  <span className={styles.avatarMessage}>there will be message</span>
+              </div>
+           </div>
+
+           <div className={styles.messageBox}>
+              <img src="/avatar.png" alt="message" className={styles.avatar}/>
+              <div className= {styles.avatarInfo}>
+                  <p className={styles.avatarName}>Rihanna Goldberg</p>
+                  <span className={styles.avatarMessage}>there will be message</span>
+              </div>
+           </div>
+
+
+        </div>
+
+
+      </div>
+    </section>
+  );
 }
