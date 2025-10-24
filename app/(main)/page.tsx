a006cfdeb9588bf7ee35a0421faca6da78281b94
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-24 21:04:59 +0400

    feat : add main page contianer

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.everythingWrapper}></div>;
+  return  (
+    <div className={styles.everythingWrapper}>
+        <div className={styles.content}></div>
+    </div>
+  )
 }
