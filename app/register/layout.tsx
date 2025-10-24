1eb1c196d54c4a27471b527741bf0a5750017811
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-24 15:37:28 +0400

    feat:  complate registation page

---
+++ app/register/layout.tsx
@@ -0,0 +1,11 @@
+import '../../app/globals.css';
+
+
+
+export default function RegisterLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div className="register-wrapper">
+        {children}
+      </div>
+    )
+  }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,42 @@
-import styles from "./register.module.scss";
+"use client";
+import styles from "./page.module.scss";
 
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <section className={styles.mainContainer}>
+
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <img src="/logo.svg" alt="logo" className={styles.logoImage} />
+        </div>
+
+        <div className={styles.formContainer}>
+           <h1 className={styles.formTitle}>Registration</h1>
+
+           <form  className={styles.form}>
+            
+            <div className={styles.formGroup}>
+              <label htmlFor="email" className={styles.formLabel}>Email</label>
+              <input className={styles.formInput}  type="email" id="email" name="email" placeholder="enter your email" />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="password" className={styles.formLabel}>Pasword</label>
+              <input className={styles.formInput} type="password" id="password" name="password" placeholder="enter your password" />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="passwordConfirm" className={styles.formLabel}>Confirm Password</label>
+              <input className={styles.formInput} type="passwordConfirm" id="passwordConfirm" name="passwordConfirm" placeholder="enter your password" />
+            </div>
+
+            <button type="button" className={styles.formButton}>Register</button>
+           </form>
+
+        </div>
+      </div>
+
+
+    </section>
+  );
 }
