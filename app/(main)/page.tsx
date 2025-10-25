dba7ed3fccf348f4b47b8b738ea5f85b0b92605a
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-10-25 19:39:37 +0400

    feat finished messenger components

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,5 @@
 import Buttons from "../components/Buttons/Buttons";
+import Messenger from "../components/Messenger/Messenger";
 import Navigation from "../components/Navigation/Navigation";
 import SideBar from "../components/SideBar/SideBar";
 import styles from "./page.module.scss";
@@ -9,6 +10,7 @@ export default function Home() {
         <div className={styles.content}>
            <Navigation/>
            <SideBar/>
+           <Messenger/>
         </div>
     </div>
   )
